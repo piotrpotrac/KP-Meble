a9f11e5387f7f7961bf8ba5e13b3809742aeb908
Piotr Potrac <piotrpotrac@gmail.com>
2022-10-03 21:02:05 +0200

    Build a about us section.

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 'use strict';
-console.log(`Hello world`);
+// Function change navigation opacity while scrolling
+const sectionNav = document.querySelector('.nav');
+
+window.addEventListener(`scroll`, function (e) {
+  const navOpac = this.scrollY / 1000;
+  if (navOpac === 0) {
+    sectionNav.style.opacity = 0.8;
+  }
+  if (navOpac > 0) {
+    sectionNav.style.opacity = `${0.8 + navOpac / 4}`;
+  }
+});
