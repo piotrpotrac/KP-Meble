fca8e91c20c1d350594721e29e45877b93619878
Piotr Potrac <piotrpotrac@gmail.com>
2022-10-25 20:53:46 +0200

    Adding a modal window and loader in js.

--- script.js
+++ script.js
@@ -1,13 +1,100 @@
 'use strict';
-// Function change navigation opacity while scrolling
-const sectionNav = document.querySelector('.nav');
 
+//Loader
+window.addEventListener('load', () => {
+  const loader = document.querySelector('.loader');
+
+  loader.classList.add('loader__hidden');
+
+  loader.addEventListener('transitionend', () => {
+    document.body.removeChild('loader');
+  });
+});
+
+//Hide nav bg
+
+const sectionNav = document.querySelector('.nav');
 window.addEventListener(`scroll`, function (e) {
   const navOpac = this.scrollY / 1000;
   if (navOpac === 0) {
-    sectionNav.style.opacity = 0.8;
+    sectionNav.classList.remove('nav__background-gradient');
   }
   if (navOpac > 0) {
-    sectionNav.style.opacity = `${0.8 + navOpac / 4}`;
+    // sectionNav.style.opacity = `${0.8 + navOpac / 4}`;
+    sectionNav.classList.add('nav__background-gradient');
+  }
+});
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  // direction: 'vertical',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
+
+//Reveal page
+
+const allSections = document.querySelectorAll('.section');
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+});
+
+// Modal window
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.btn--close-modal');
+const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+
+const openModal = function (e) {
+  e.preventDefault();
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
   }
 });
