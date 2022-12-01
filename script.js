3da463d586f1c9c96a35c4bab8fff284dfb02043
Piotr Potrac <piotrpotrac@gmail.com>
2022-12-01 22:57:09 +0100

    Fixed landscape mediaquery.

--- script.js
+++ script.js
@@ -14,14 +14,20 @@ window.addEventListener('load', () => {
 //Hide nav bg
 
 const sectionNav = document.querySelector('.nav');
+const hideLogo = document.querySelector('#logo__hide');
+
 window.addEventListener(`scroll`, function (e) {
   const navOpac = this.scrollY / 1000;
+
   if (navOpac === 0) {
     sectionNav.classList.remove('nav__background-gradient');
+    hideLogo.classList.remove('logo__hide');
   }
+
   if (navOpac > 0) {
     // sectionNav.style.opacity = `${0.8 + navOpac / 4}`;
     sectionNav.classList.add('nav__background-gradient');
+    hideLogo.classList.add('logo__hide');
   }
 });
 
