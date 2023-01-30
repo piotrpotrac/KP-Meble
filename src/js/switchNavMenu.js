93c65289a84a7479377ba5be53252a5937e034b9
Piotr Potrac <piotrpotrac@gmail.com>
2023-01-30 10:51:39 +0100

    chore: renaming folders in src folder.

--- src/js/switchNavMenu.js
+++
@@ -1,6 +0,0 @@
-export const switchNavMenu = () => {
-  const checkbox = document.querySelector('#nav__checkbox')
-  const navMenu = document.querySelector('.nav__menu')
-
-  navMenu.addEventListener('click', () => (checkbox.checked = false))
-}
